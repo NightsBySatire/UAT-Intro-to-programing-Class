dfd3f7ec92fa237427397f534f21f034136e96d5
NightsBySatire <mrtj6@yahoo.com>
2021-12-13 10:09:30 -0700

    Submission

--- Buttons/main.js
+++ Buttons/main.js
@@ -1,13 +1,13 @@
-// Create the function to enable stop button and disable start buton
+// Creating our functions, note our lack of variables due to the fact that we don't really have anything to do in this function other than set true/false values
 function start(){
-    //change the start button  by getElementByID. Disable it by making value trye
+    //Grabbing our element and changing its value to what each button should reflect on either start or stop
     document.getElementById("startButton").disabled = true;
     //Similar to top portion, except false to enabe
     document.getElementById("stopButton").disabled = false;
 }
-//Create function to enable start and disable stop
+//Creating our second function for the stop 
 function stop(){
-    //Similar to top function
+    //Inverse of our start function
     document.getElementById("startButton").disabled = false;
     document.getElementById("stopButton").disabled = true;
 }
